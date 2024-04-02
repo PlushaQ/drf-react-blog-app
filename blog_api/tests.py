85165b5cad041c5642c21b7adccce198d2b97b7e
Maciej Fularz <fularzmaciej@gmail.com>
2024-04-02 18:54:30 +0200

    Added simple tests

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from blog.models import Post, Category
+
+class PostTests(APITestCase):
+
+    def test_view_posts(self):
+        url = reverse('blog_api:list_create')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def create_post(self):
+        self.test_category = Category.objects.create(name='django')
+        self.test_user = User.objects.create_user(username='test_user', password='123456')
+        data = {'title': 'new', 'author': 'new', 'excerpt': 'new', 'content': 'new',}
+        url = reverse('blog_api:list_create')
+        response = self.client.post(url, data, format='json',)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostDetail, PostList
+
 app_name = 'blog_api'
 
 urlpatterns = [
