a3b7825c427c4b94890e1a3816d4b03c3af0ed65
Maciej Fularz <fularzmaciej@gmail.com>
2024-04-09 21:30:17 +0200

    Added permission tests

--- drf_backend/blog_api/tests.py
+++ drf_backend/blog_api/tests.py
@@ -1,21 +1,65 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from django.contrib.auth.models import User
 
 from blog.models import Post, Category
 
 class PostTests(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.test_user = User.objects.create_user(username='test_user', password='123456')
+        self.test_user2 = User.objects.create_user(username='test_user2', password='123456')
+        self.test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='Post excerpt', content='Post content', slug='post-title', author_id=1, status='published')
 
     def test_view_posts(self):
         url = reverse('blog_api:list_create')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def create_post(self):
-        self.test_category = Category.objects.create(name='django')
-        self.test_user = User.objects.create_user(username='test_user', password='123456')
-        data = {'title': 'new', 'author': 'new', 'excerpt': 'new', 'content': 'new',}
+    def test_create_post(self):
+        self.client.login(username=self.test_user.username, password='123456')
+
+        data = {'title': 'new', 'author': 1, 'excerpt': 'new', 'content': 'new',}
         url = reverse('blog_api:list_create')
+
         response = self.client.post(url, data, format='json',)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_post_update_by_owner_user(self):
+        self.client.login(username=self.test_user.username, password='123456')
+
+        url = reverse(('blog_api:detail_create'), kwargs={'pk': 1})
+
+        response = self.client.put(url, {
+            "id": 1,
+            "title": "New",
+            "author": 1,
+            "excerpt": "new",
+            "content": "new",
+            "status": "published"
+        }, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_post_update_by_not_owner_user(self):
+        self.client.login(username=self.test_user2.username, password='123456')
+
+        url = reverse(('blog_api:detail_create'), kwargs={'pk': 1})
+
+        response = self.client.put(url, {
+            "id": 1,
+            "title": "New",
+            "author": 1,
+            "excerpt": "new",
+            "content": "new",
+            "status": "published"
+        }, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+        
+        
--- drf_backend/blog_api/views.py
+++ drf_backend/blog_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, SAFE_METHODS
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly, SAFE_METHODS
 
 
 class PostUserWritePermission(BasePermission):
@@ -14,7 +14,7 @@ class PostUserWritePermission(BasePermission):
         return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
