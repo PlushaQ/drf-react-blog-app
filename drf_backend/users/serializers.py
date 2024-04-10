a2d5c7b093b39431c6833bbf55b9ccb6df39e501
Maciej Fularz <fularzmaciej@gmail.com>
2024-04-10 21:28:44 +0200

    Added urls to user create

--- drf_backend/users/serializers.py
+++ drf_backend/users/serializers.py
@@ -11,7 +11,7 @@ class RegisterUserSerializer(ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
-        if password is not None:
+        if password:
             instance.set_password(password)
         instance.save()
         return instance
